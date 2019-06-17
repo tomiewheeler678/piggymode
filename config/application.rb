4b2ad4c5f2c0cfeecd962db2f67e2859f0f2d872
tomiewheeler678 <tomiewheeler678@gmail.com>
2019-06-17 22:57:39 +0000

    splurty heroku

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Splurty
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-# Add the fonts path
+    # Add the fonts path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
